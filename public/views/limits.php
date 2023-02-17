98537ef1148050dd547af9e4c1e3e0a6715204a2
WojciechMalik <wojciech.malik@student.pk.edu.pl>
2023-02-17 06:45:11 +0100

    fixed limits styling

--- public/views/limits.php
+++ public/views/limits.php
@@ -11,31 +11,29 @@
         <div class="base-container">
             <?php include "navigation.php"?>
             <main>
-                <h1>Limits</h1>
-                <h2 id="h1">Category</h2>
-                <h2 id="h2">Amount</h2>
-                <h2 id="h3">New</h2>
                 <form action="setLimits" method="post">
-                    <div class="limit-container food">
-                        <p class="name">Food</p>
-                        <p class="current">$<?=$limits[0]->getLimit()?></p>
-                        <input placeholder="$" class="new" name="Food">
-                    </div>
-                    <div class="limit-container entertainment ">
-                        <p class="name">Entertainment</p>
-                        <p class="current">$<?=$limits[1]->getLimit()?></p>
-                        <input placeholder="$" class="new" name="Entertainment">
-                    </div>
-                    <div class="limit-container education ">
-                        <p class="name">Education</p>
-                        <p class="current">$<?=$limits[2]->getLimit()?></p>
-                        <input placeholder="$" class="new" name="Education">
-                    </div>
-                    <div class="limit-container transport">
-                        <p class="name">Transport</p>
-                        <p class="current">$<?=$limits[3]->getLimit()?></p>
-                        <input placeholder="$" class="new" name="Transport">
-                    </div>
+                    <h1>Limits</h1>
+                    <h2 id="h1">Category</h2>
+                    <h2 id="h2">Amount</h2>
+                    <h2 id="h3">New</h2>
+
+                    <p class="food name">Food</p>
+                    <p class="food current">$<?=$limits[0]->getLimit()?></p>
+                    <input placeholder="$" class="food new" name="Food">
+
+                    <p class="entertainment name">Entertainment</p>
+                    <p class="entertainment current">$<?=$limits[1]->getLimit()?></p>
+                    <input placeholder="$" class="entertainment new" name="Entertainment">
+
+                    <p class="education name">Education</p>
+                    <p class="education current">$<?=$limits[2]->getLimit()?></p>
+                    <input placeholder="$" class="education new" name="Education">
+
+
+                    <p class="transport name">Transport</p>
+                    <p class="transport current">$<?=$limits[3]->getLimit()?></p>
+                    <input placeholder="$" class="transport new" name="Transport">
+
                     <button type="submit" id="save">Save</button>
                 </form>
             </main>
