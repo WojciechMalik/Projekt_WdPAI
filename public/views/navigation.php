58806ee311d2259b58291b96cbc34990bc42f3c8
WojciechMalik <wojciech.malik@student.pk.edu.pl>
2023-02-17 11:18:37 +0100

    added media queries

--- public/views/navigation.php
+++ public/views/navigation.php
@@ -1,3 +1,4 @@
+<h3><span>On</span>Budget</h3>
 <nav>
     <h1><span>On</span>Budget</h1>
     <ul>
@@ -33,3 +34,24 @@
         </li>
     </ul>
 </nav>
+<div class="mobile-helper">
+    <button onclick="window.location.href='newtransaction';" class="newtransaction">
+        <img src="public/img/transaction-icon.svg" alt="new transaction icon">
+    </button>
+    <button onclick="window.location.href='dashboard';">
+        <img src="public/img/dashboard-icon.svg" alt="dashboard icon">
+
+    </button>
+    <button onclick="window.location.href='statistics';">
+        <img src="public/img/statistics-icon.svg" alt="stats icon">
+
+    </button>
+    <button onclick="window.location.href='limits';">
+        <img src="public/img/limits-icon.svg" alt="limits icon">
+
+    </button>
+    <button onclick="window.location.href='logout';">
+        <img src="public/img/logout-icon.svg" alt="logout icon">
+
+    </button>
+</div>
